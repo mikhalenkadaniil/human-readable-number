f9f059735dbfaaf0608372930efdf9617e0238a2
mikhalenkadaniil <mikhalenka.d@gmail.com>
2025-07-31 16:18:22 +0200

    feat: function toReadable

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
-module.exports = function toReadable(/* number */) {
-  throw new Error('Not implemented');
+module.exports = function toReadable(number) {
+  if (number === 0) return 'zero';
+  const UNITS = [
+    '',
+    'one',
+    'two',
+    'three',
+    'four',
+    'five',
+    'six',
+    'seven',
+    'eight',
+    'nine',
+    'ten',
+    'eleven',
+    'twelve',
+    'thirteen',
+    'fourteen',
+    'fifteen',
+    'sixteen',
+    'seventeen',
+    'eighteen',
+    'nineteen',
+  ];
+  const TENS = [
+    '',
+    '',
+    'twenty',
+    'thirty',
+    'forty',
+    'fifty',
+    'sixty',
+    'seventy',
+    'eighty',
+    'ninety',
+  ];
+  let unit = UNITS[number % 10];
+  if (number % 100 < 20) {
+    unit = UNITS[number % 100];
+  }
+  const hundred = UNITS[(number - (number % 100)) / 100];
+  const ten = TENS[((number % 100) - (number % 10)) / 10];
+  const result = [];
+  if (hundred !== '') result.push(`${hundred} hundred`);
+  if (ten !== '') result.push(ten);
+  if (unit !== '') result.push(unit);
+  return result.join(' ');
 };
